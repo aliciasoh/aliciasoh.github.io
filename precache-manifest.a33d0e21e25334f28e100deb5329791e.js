0c6d49e82bfbeace0916affc3c765b8f6453a783
Alicia Soh <aliciasoh@Alicias-MacBook-Pro>
2020-04-25 13:26:01 +0200

    Updates

---
+++ precache-manifest.a33d0e21e25334f28e100deb5329791e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95615d93a49ba05bd8b616ee6d4933a1",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "4ebc111f32a21a4f5899",
+    "url": "/website/static/css/main.5450793a.chunk.css"
+  },
+  {
+    "revision": "d32c06840e6fb83a5ca1",
+    "url": "/website/static/js/2.0e3ada2c.chunk.js"
+  },
+  {
+    "revision": "4ebc111f32a21a4f5899",
+    "url": "/website/static/js/main.ef36a845.chunk.js"
+  },
+  {
+    "revision": "cbafe1f1289291854a43",
+    "url": "/website/static/js/runtime~main.5b19542e.js"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/website/static/media/landing-bg.df8fd3ef.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.216f62e80ea45d42ac4e9a8942dbed7f.js"
+  "/website/precache-manifest.a33d0e21e25334f28e100deb5329791e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
